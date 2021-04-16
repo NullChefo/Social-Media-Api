52bb484c4210945533f9241017d7a0779c665910
StefanKehayov <stefan.kehayov.96@gmail.com>
2021-04-16 19:16:57 +0300

    Added encruption library

---
+++ SMA.Data/Contexts/SmaDbContex.cs
@@ -0,0 +1,24 @@
+﻿namespace SMA.Data.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using Entities;
+
+    public class SMAContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public SMAContext() : base() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Nlear;User Id=applogin;Password=1234;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Role>().ToTable("Role");
+        }
+    }
+}
---
+++ SMA.Data/Entities/Entity.cs
@@ -0,0 +1,30 @@
+﻿namespace SMA.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class Entity
+    {
+        #region Constructors
+        public Entity()
+        {
+            CreatedOn = DateTime.Now;
+        }
+        #endregion
+
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        #endregion
+    }
+}
